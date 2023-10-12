a0e5566afbef708030b2f03318939a971af8485c
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-12 23:18:11 +0530

    Added variable file where var,let,const are described

---
+++ 1_hello.js
@@ -0,0 +1,4 @@
+console.log('Hello World!')
+console.log("A+B = "+(4+2)) // A+B = 6
+
+//Concatenate with string will make the entire result as a string
---
+++ 2_Variable.js
@@ -0,0 +1,16 @@
+console.log("10+15 = " + (10+15)) //10+15 = 25 
+
+//Variable needs a data type but JS is a high level data type so we donot have to mention the type of variable, compiler automatically understands it
+
+// To declare a varibale var keyword are used before but nowadays we donot use var 
+var a = 5
+//Nowadays we use let to declare a variable
+let b = 3
+let c = 5
+console.log("b+c="+ (b+c)) //b+c= 8
+
+//const to declare a constant variable whose value will not be updated after initialization
+const PI = 3.14
+
+// NOTE 
+//Variable is used to resuable data/value
