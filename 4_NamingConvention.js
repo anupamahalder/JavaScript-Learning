212372796f91f241561211e2f55641d136b9aebf
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-16 23:42:04 +0530

    Added details about naming convention in javascript

---
+++ 4_NamingConvention.js
@@ -0,0 +1,15 @@
+// camel case (mostly used in JS)
+const myAge = 22;
+console.log(myAge);
+
+//snake case (mostly used in c/c++/python/php)
+const my_address = "Kolkata, India";
+console.log(my_address);
+
+/**
+ * NOTE : 1. Class name has to be started with capital letter 
+ * 2. reserved word cannot be used as a varible name
+ */
+
+// Camel case: first letter will be lowercase and remaining word's first letter will be upper case
+// Snake case: words will be concatenated with uderscore(_) and each word will be in lower case 
