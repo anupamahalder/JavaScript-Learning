91e8c15fc1145c7a087a040f4c124e347788655d
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-17 00:03:56 +0530

    Added the uses of Arithmetic and Assignment operator

---
+++ 5_Operator.js
@@ -0,0 +1,43 @@
+// Using operator we can perform some operations 
+// Operator perform operations of operand(operands)
+// result = 23 + 5 (Here, result,23,5 are operands and =,+ are operators)
+
+/**
+ *  Types of operators---
+ *  1. Arithmetic Operator =>
+ *           Addition(+),   
+ *           Subtraction(-), 
+ *           Multiplication(*), 
+ *           Division(/),
+ *           Modulus(%)
+ * 
+ *  2. Assignment Oprator => 
+ *           Equal(=),
+ *           Plus Equal(+=),
+ *           Minus Equal(-=),
+ *           Into Equal(*=),
+ *           Division Equal(/=),
+ *           Modulus Equal(%=),
+ *  
+ *  3. Relational Oprator =>
+ *  4. Logical Oprator =>
+ *  5. Increment Decrement Operator =>
+ *  6. Bitwise Operator => or(||), and(&&), not(!)
+ *  
+*/
+
+// Arithmetic Operator 
+const a = 5 + 4; // 9
+const b = 5 - 4; // 1
+const c = 5 * 4; // 20
+const d = 5 / 4; // 1.25
+const e = 5 % 4; // 1
+
+// Assignment operator 
+let A = 10; // 10
+A += 5; // 15
+A -= 4; // 11
+A *= 6; // 66
+A /= 5; // 6.19999999999
+A %= 7; // 6.19999999999
+
