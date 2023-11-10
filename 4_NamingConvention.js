a15be27cea0a276e1fe61845ab7d4cbdd97c3ef8
Anupama_Halder <anupamahalder2022@gmail.com>
2023-11-10 23:40:28 +0530

    Added naming convention while writing javascript variable

--- 4_NamingConvention.js
+++ 4_NamingConvention.js
@@ -9,6 +9,7 @@ console.log(my_address);
 /**
  * NOTE : 1. Class name has to be started with capital letter 
  * 2. reserved word cannot be used as a varible name
+ * 3. variable name cannot be started with a number
  */
 
 // Camel case: first letter will be lowercase and remaining word's first letter will be upper case
