e7f8ccaa1c5aab59cb73595c5ca686290dd83dc3
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-03-13 21:06:38 +0530

    Added declarations of variables and their modification based on scope

--- 2_Variable.js
+++ 2_Variable.js
@@ -1,3 +1,23 @@
+const accountId = 234212;
+let accountEmail = "xyz@gmail.com";
+var accountPassword = "12345";
+accountCity = "Kolkata";
+let accountState;
+
+// modify the variables 
+// accountId = 2;
+accountEmail = "abc@gmail.com";
+accountPassword = "1234555";
+accountCity = "Bangaluru";
+
+// to see maultiple values of varibles in tabular format 
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
+
+
+/**
+ * Note: Prefer not to use var
+ */
+
 console.log("10+15 = " + (10+15)) //10+15 = 25 
 
 //Variable needs a data type but JS is a high level data type so we donot have to mention the type of variable, compiler automatically understands it
