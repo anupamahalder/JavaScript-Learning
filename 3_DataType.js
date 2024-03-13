ad4a0c70a851dc0a933f251f68aeea54be61a13b
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-03-13 21:24:10 +0530

    Types of data types in js

--- 3_DataType.js
+++ 3_DataType.js
@@ -29,3 +29,28 @@ console.log(typeof g); //object
  * 2. When we don't know what kind of data a variable will have then we can initialize with null. and the type of this variable is object as it is mainly used for objects. And it one of the concept of Object-Oriented Programming (OOP).
  * */  
 
+"use strict"; //treat all JS code as newer version of JS
+
+// alert("Hello"); //Here we are getting error bcz we use node js not browser to run alert
+
+let name = "Anupama"
+let age = 20
+
+/**  -------------Primitive Datatypes------------
+ * number => 2 to power 53
+ * bigint
+ * string => ""
+ * boolean => true/false
+ * null => standalone value
+ * undefined => value not provided
+ * symbol => unique
+*/
+
+/** -------------Non-Primitives Datatypes---------
+ * Object
+ */
+
+console.log(typeof "Hello"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
