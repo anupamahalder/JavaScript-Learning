924c5b92e59de796f86f02a3b6938dadb0ca7273
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-03-15 00:03:11 +0530

    Other datatype to string conversion in js

--- 009_Datatype_to_String_conversion.js
+++ 009_Datatype_to_String_conversion.js
@@ -3,3 +3,21 @@ let num = 30;
 let stringNum = String(num);
 console.log(stringNum); //30
 console.log(typeof stringNum); // string
+
+// --------Boolean to string conversion------
+let myBool = false;
+let myBoolString = String(myBool);
+console.log(myBoolString); // false
+console.log(typeof myBoolString); // string
+
+// --------Undefined to string conversion ----
+let myUndefined = undefined;
+let myUndefinedString = String(myUndefined);
+console.log(myUndefinedString); // Undefined
+console.log(typeof myUndefinedString); // string
+
+// ----------null to string conversion-------
+let myNull = null;
+let myNullString = String(myNull);
+console.log(myNullString); // null
+console.log(typeof myNullString); // string
