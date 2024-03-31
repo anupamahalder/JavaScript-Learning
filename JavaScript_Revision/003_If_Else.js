d90e69f6d016bac8c7b9b778d4bf6c54fe1a052c
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-03-31 19:03:07 +0530

    Checking conditions using If else block

---
+++ JavaScript_Revision/003_If_Else.js
@@ -0,0 +1,17 @@
+/**
+ * IF-ELSE CONDITION:
+ * If 'if' condition doesn't satisfy then it will go to 'else' part
+ */
+
+var applePrice = 10;
+var biriyaniPrice = 100;
+
+if(applePrice < biriyaniPrice){
+    console.log('Apple is cheaper!');
+}
+else if(applePrice == biriyaniPrice){
+    console.log('Biriyani is same priced as Apple!');
+}
+else{
+    console.log('Apple is cheaper!');
+}
