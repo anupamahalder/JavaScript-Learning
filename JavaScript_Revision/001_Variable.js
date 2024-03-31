c842216c5af43083a705fdbc9bf6fc2b1bcdf36f
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-03-31 18:21:45 +0530

    Added variables in JavaScript and their naming convention

---
+++ JavaScript_Revision/001_Variable.js
@@ -0,0 +1,42 @@
+/**
+ * VARIABLE ==> container
+ */
+
+// number 
+var price = 100;
+
+// string 
+var subject = "CSE";
+
+// boolean
+var passed = true;
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no qoute
+ * 3. no gap or dash
+ * 4. cannot start with number
+ * 5. cannot be a keyword
+ * 6. prefer camelcase
+ */
+
+var myScore = 8.9;
+
+
+/**
+ * OPERATION:
+ * 1. +, -,*, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN ?
+ * 4. null
+ * 5. Number
+ */
+
+/**
+ * NOTE:
+ * In JavaScript, the isNaN() function is used to check whether a value is NaN,
+ *  which stands for "Not a Number". It returns true if the value is NaN, and
+ *  false otherwise. The isNaN() function can be used on any value, but it is 
+ *  most commonly used on string values that are expected to be numbers.
+ */
